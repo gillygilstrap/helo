138e6a9b3c0643ec322c10e84d006f2231018e74
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-25 22:32:11 -0700

    Component Structure Set

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { loopback } from 'ip';
+import Nav from './components/Nav/Nav'
+import Post from './components/Post/Post'
+import Dashboard from './components/Dashboard/Dashboard'
+import Auth from './components/Auth/Auth'
+import Form from './components/Form/Form'
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Post />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
